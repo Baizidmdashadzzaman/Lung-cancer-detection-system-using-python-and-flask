ac5bc03926804ae4c2264391a0ad0c2ec8ee06e2
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2024-06-03 12:33:50 +0600

    06-03-2024

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+import pickle
+import numpy as np
+import pandas as pd
+import os
+import sys
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash, app, jsonify
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
+classifier = pickle.load(open('model.pkl', 'rb'))
+
 @app.route('/')
 def home():
+
     title = "Home"
     return render_template('index.html',title=title)
 @app.route('/about')
@@ -68,6 +77,31 @@ def logout():
     session.pop('user', None)
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # data = [float(x) for x in request.form.values()]
+    # final_input = np.array(data).reshape(1, -1)
+    print(request.form['level'])
+    sys.exit()
+    # output = regmodel.predict(final_input)[0]
+    # return render_template("home.html",
+    # prediction_text=f"The House Price Prediction is {output} hundreds of thousands of dollars")
+    title = "If you dont have lung cancer"
+    return render_template('patient/dashboard.html',title=title)
+
 
 
 if __name__ == '__main__':
